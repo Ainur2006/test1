0c26e92e0beb96607b65e2c88ab61e2fcf49e04d
Айнур Халилов <xalilovajnur@gmail.com>
2026-03-07 17:36:02 +0300

    добавил комментарии и

--- bubble_sort.py
+++ bubble_sort.py
@@ -7,11 +7,24 @@ def bubble_sort(incoming_list: list = []) -> list:
         swap = False
         for i in range(count_step):
             if incoming_list[i] > incoming_list[i + 1]:
-                incoming_list[i], incoming_list[i + 1] = incoming_list[i + 1], incoming_list[i] # меняем местами пары чисел, большее становится правее
-                swap = True
-        count_step -= 1 # уменьшаем счётчик, т.к. большее число в конце списка
+                incoming_list[i], incoming_list[i + 1] = (
+                    incoming_list[i + 1],
+                    incoming_list[i],
+                )  # меняем местами пары чисел, большее становится правее
+                swap = True  # если произошла замена, то меняем значение "swap"
+        count_step -= 1  # уменьшаем счётчик, т.к. большее число в конце списка
     return incoming_list
 
-list = [2, 5, 2, 8, 76, 4, 6,]
+
+list = [
+    2,
+    5,
+    2,
+    8,
+    76,
+    4,
+    6,
+]
 
 print(bubble_sort(list))
+print()
